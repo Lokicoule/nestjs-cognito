699fe12d0698df1b58233b63cf5862839b6d3a18
lokicoule <lokicoule@yahoo.fr>
2022-07-24 22:53:43 +0200

    fix(sample): cleanup

--- sample/01-auth/src/whale/whale.controller.ts
+++ sample/01-auth/src/whale/whale.controller.ts
@@ -1,12 +1,11 @@
-import { AuthorizationGuard, CurrentUser, User } from '@nestjs-cognito/auth';
+import { AuthorizationGuard } from '@nestjs-cognito/auth';
 import { Controller, Get, UseGuards } from '@nestjs/common';
 
 @Controller('whale')
 export class WhaleController {
   @Get('moby')
   @UseGuards(AuthorizationGuard(['admin', 'whale']))
-  getMoby(@CurrentUser() user: User) {
-    console.log(user);
+  getMoby() {
     return {
       message: 'Moby',
     };
