072b1c091432ddd2a0952691e6bf0e8f5ea89436
lokicoule <lokicoule@yahoo.fr>
2022-07-25 11:54:39 +0200

    test(testing): e2e

---
+++ packages/testing/tests/app.e2e.spec.ts
@@ -0,0 +1,56 @@
+import { INestApplication } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { Test } from "@nestjs/testing";
+import { request, spec } from "pactum";
+import { AppModule } from "./app.module";
+
+describe("Cognito Module : Testing", () => {
+  let app: INestApplication;
+  let config: ConfigService;
+
+  beforeAll(async () => {
+    const moduleFixture = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    config = moduleFixture.get<ConfigService>(ConfigService);
+
+    await app.listen(0);
+    const url = (await app.getUrl()).replace("[::1]", "localhost");
+    request.setBaseUrl(url);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be defined", () => {
+    expect(app).toBeDefined();
+  });
+
+  describe("login", () => {
+    it("should be successfull and return http code 2O1", async () => {
+      await spec()
+        .post("/cognito-testing-login")
+        .withBody({
+          username: config.get("COGNITO_USER_EMAIL"),
+          password: config.get("COGNITO_USER_PASSWORD"),
+          clientId: config.get("COGNITO_CLIENT_ID"),
+        })
+        .expectStatus(201)
+        .expectBodyContains("AccessToken");
+    });
+
+    it("should be unsuccessful and return http code 403", async () => {
+      await spec()
+        .post("/cognito-testing-login")
+        .withBody({
+          username: "John Doe",
+          password: "123456",
+          clientId: config.get("COGNITO_CLIENT_ID"),
+        })
+        .expectStatus(401);
+    });
+  });
+});
---
+++ packages/testing/tests/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { CognitoTestingModule } from "../lib/cognito-testing.module";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    CognitoTestingModule.register({
+      region: "eu-west-1",
+    }),
+  ],
+})
+export class AppModule {}
