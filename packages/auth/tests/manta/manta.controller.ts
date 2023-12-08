1ea77912465d6847c21fca783a10985f7e81ab90
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-12-08 18:27:17 +0100

    refactor: authentication and authorization method decorators

--- packages/auth/tests/manta/manta.controller.ts
+++ packages/auth/tests/manta/manta.controller.ts
@@ -2,12 +2,12 @@ import { Authorization } from "../../lib";
 import { Controller, Get } from "@nestjs/common";
 
 @Controller("manta")
-@Authorization({
-  requiredGroups: ["manta"],
-  prohibitedGroups: ["dolphin", "shark", "whale"],
-})
 export class MantaController {
   @Get("ray")
+  @Authorization({
+    requiredGroups: ["manta"],
+    prohibitedGroups: ["dolphin", "shark", "whale"],
+  })
   getRay() {
     return {
       message: "Ray",
--- packages/graphql/lib/authentication/authentication.decorator.ts
+++ packages/graphql/lib/authentication/authentication.decorator.ts
@@ -2,18 +2,25 @@ import { applyDecorators, UseGuards } from "@nestjs/common";
 import { AuthenticationGuard } from "./authentication.guard";
 
 /**
- * @deprecated This decorator is deprecated. Use GqlAuthentication instead. This decorator will be removed in the next major release.
  * Decorator for the AuthenticationGuard
  * @returns {ClassDecorator} - The decorator
  */
-export function Authentication(): ClassDecorator {
-  return applyDecorators(UseGuards(AuthenticationGuard));
-}
-
-/**
- * Decorator for the AuthenticationGuard
- * @returns {ClassDecorator} - The decorator
- */
-export function GqlAuthentication(): ClassDecorator {
-  return applyDecorators(UseGuards(AuthenticationGuard));
+export function GqlAuthentication(): ClassDecorator & MethodDecorator {
+  return (
+    target: object,
+    propertyKey?: string | symbol,
+    descriptor?: TypedPropertyDescriptor<any>,
+  ) => {
+    if (propertyKey && descriptor) {
+      // MethodDecorator
+      return applyDecorators(UseGuards(AuthenticationGuard))(
+        target,
+        propertyKey,
+        descriptor,
+      );
+    } else {
+      // ClassDecorator
+      return applyDecorators(UseGuards(AuthenticationGuard))(target);
+    }
+  };
 }
