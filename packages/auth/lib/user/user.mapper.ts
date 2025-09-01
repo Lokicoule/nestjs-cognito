fcfc974d9e46364214c552c24d5dd9178386fcce
Loïk Fekkaï <lokicoule@yahoo.fr>
2025-09-01 18:59:56 +0200

    feat: support client credentials flow authentication (#1401)
    * feat: support client credentials flow without username requirement
    
    * fix: update user email expectation in UserMapper tests to beUndefined

--- packages/auth/lib/user/user.mapper.ts
+++ packages/auth/lib/user/user.mapper.ts
@@ -1,37 +1,53 @@
+import type { CognitoJwtPayload } from "@nestjs-cognito/core";
 import { UnauthorizedException } from "@nestjs/common";
 import { UserBuilder } from "./user.builder";
 import { User } from "./user.model";
 
-import type { CognitoJwtPayload, JwtPayload } from "aws-jwt-verify/jwt-model";
-
 export class UserMapper {
   private static readonly ERRORS = {
-    USERNAME: "User must have a username",
+    USERNAME: "User must have a username or client ID",
   };
 
+
+
   public static fromCognitoJwtPayload(
-    payload: JwtPayload | CognitoJwtPayload,
+    payload: CognitoJwtPayload,
   ): User {
     const username = payload["cognito:username"] || payload["username"] || null;
+    const clientId = payload["client_id"]?.toString() || null;
     const email = payload["email"]?.toString() ?? null;
+    const isClientCredentialsFlow = payload["token_use"] === "access" && payload["scope"] && clientId;
 
-    if (!username) {
+    if (isClientCredentialsFlow) {
+      const groups = this.extractGroups(payload);
+      return new UserBuilder()
+        .setClientId(clientId)
+        .setEmail(email)
+        .setGroups(groups)
+        .build();
+    }
+
+    if (!(username)) {
       throw new UnauthorizedException(UserMapper.ERRORS.USERNAME);
     }
 
-    let groups: string[] = [];
-    const payloadGroups = payload["cognito:groups"] || payload["groups"];
-
-    if (Array.isArray(payloadGroups)) {
-      groups = payloadGroups.map((group) => group.toString());
-    } else if (payloadGroups) {
-      groups = [payloadGroups.toString()];
-    }
+    const groups = this.extractGroups(payload);
 
     return new UserBuilder()
       .setUsername(username.toString())
       .setEmail(email)
       .setGroups(groups)
       .build();
+  }
+
+  private static extractGroups(payload: CognitoJwtPayload): string[] {
+    const payloadGroups = payload["cognito:groups"] || payload["groups"];
+
+    if (Array.isArray(payloadGroups)) {
+      return payloadGroups.map((group) => group.toString());
+    } else if (payloadGroups) {
+      return [payloadGroups.toString()];
+    }
+    return [];
   }
 }
